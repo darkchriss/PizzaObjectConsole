2a4e9c70da4592055239778fea0312bcc0a62ac4
schro <schro@DARKCHRISS-PC>
2022-11-12 12:40:24 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PizzaObjectConsole
+{
+    internal class Program
+    {
+        // Nom, prix, Afficher()
+
+        class Pizza
+        {
+            string nom;
+            int prix;
+
+            // Constructeur
+            public Pizza(string nom, int prix)
+            {
+                this.nom = nom;
+                this.prix = prix;
+            }
+
+            public void Afficher()
+            {
+                Console.WriteLine("Pizza: " + nom + " - " + prix + "€");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Pizza maPizza = new Pizza("quatre fromages", 11);
+            maPizza.Afficher();
+        }
+    }
+}
