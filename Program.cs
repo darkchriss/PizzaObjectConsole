81f0603baa37a72e153789074ae9330e86f74203
Darkchriss <68563449+darkchriss@users.noreply.github.com>
2022-11-13 17:20:04 +0100

    Update program.cs

--- Program.cs
+++ Program.cs
@@ -1,28 +1,96 @@
-﻿namespace PizzaObjectConsole
+namespace PizzaObjetConsole
 {
-    internal class Program
+    class MainClass
     {
         // Nom, prix, Afficher()
 
         class Pizza
         {
-            string nom;
-            int prix;
+
+            private static Pizza dernierePizzaCree;
+            private static Pizza pizzaLaMoinsChere;
+            private static Pizza pizzaLaPlusChere;
+
+            private string nom;
+            private int prix;
 
             // Constructeur
             public Pizza(string nom, int prix)
             {
                 this.nom = nom;
                 this.prix = prix;
+
+                dernierePizzaCree = this;
+
+                if (pizzaLaMoinsChere == null)
+                {
+                    pizzaLaMoinsChere = this;
+                }
+                else
+                {
+                    if (prix < pizzaLaMoinsChere.Prix)
+                    {
+                        pizzaLaMoinsChere = this;
+                    }
+                }
+
+                if (pizzaLaPlusChere == null)
+                {
+                    pizzaLaPlusChere = this;
+                }
+                else
+                {
+                    if (prix > pizzaLaPlusChere.Prix)
+                    {
+                        pizzaLaPlusChere = this;
+                    }
+                }
             }
 
             public void Afficher()
             {
                 Console.WriteLine("Pizza: " + nom + " - " + prix + "€");
             }
+
+            public int Prix
+            {
+                get { return prix; }
+            }
+
+            public static void AfficherDernierePizza()
+            {
+                if (dernierePizzaCree == null)
+                {
+                    Console.WriteLine("ERREUR: Vous n'avez pas crée de pizza");
+                }
+                else
+                {
+                    Console.WriteLine("** DERNIERE PIZZA CREE**");
+                    dernierePizzaCree.Afficher();
+                }
+            }
+
+            public static void AfficherPizzaMoisChereEtPlusChere()
+            {
+                if (pizzaLaMoinsChere != null)
+                {
+                    Console.WriteLine("** PIZZA LA MOINS CHERE **");
+                    pizzaLaMoinsChere.Afficher();
+                }
+                if (pizzaLaPlusChere != null)
+                {
+                    Console.WriteLine("** PIZZA LA PLUS CHERE **");
+                    pizzaLaPlusChere.Afficher();
+                }
+            }
+
         }
-        static void Main(string[] args)
+
+
+        public static void Main(string[] args)
         {
+
+
             List<Pizza> pizzas = new List<Pizza>();
             pizzas.Add(new Pizza("quatre fromages", 11));
             pizzas.Add(new Pizza("norvegienne", 16));
@@ -30,10 +98,19 @@
             pizzas.Add(new Pizza("montagnarde", 15));
             pizzas.Add(new Pizza("végétarienne", 9));
 
-            foreach(Pizza p in pizzas)
+            pizzas.Sort((Pizza x, Pizza y) =>
+            {
+                return x.Prix.CompareTo(y.Prix);
+            });
+
+            foreach (Pizza p in pizzas)
             {
                 p.Afficher();
             }
+
+            Pizza.AfficherDernierePizza();
+
+            Pizza.AfficherPizzaMoisChereEtPlusChere();
         }
     }
 }
